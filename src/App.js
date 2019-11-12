1a559f6a2d0aad9b9a8eae2880539395853b6888
Emil <emilgth@gmail.com>
2019-11-12 11:42:21 +0100

    admin login FIXED

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import facade from "./apiFacade";
+
+function LogIn({login}) {
+	const init = {username: "", password: ""};
+	const [loginCredentials, setLoginCredentials] = useState(init);
+
+	const performLogin = (evt) => {
+		evt.preventDefault();
+		login(loginCredentials.username, loginCredentials.password);
+	}
+	const onChange = (evt) => {
+		setLoginCredentials({...loginCredentials, [evt.target.id]: evt.target.value})
+	}
+
+	return (
+		<div>
+			<h2>Login</h2>
+			<form onChange={onChange}>
+				<input placeholder="User Name" id="username"/>
+				<input placeholder="Password" id="password"/>
+				<button onClick={performLogin}>Login</button>
+			</form>
+		</div>
+	)
+
+}
+
+function LoggedIn({user}) {
+	const [dataFromServer, setDataFromServer] = useState("Loading...")
+
+	useEffect(() => {
+		facade.fetchData(user).then(data => setDataFromServer(data.msg));
+	}, [user])
+
+	return (
+		<div>
+			<h2>Data Received from server</h2>
+			<h3>{dataFromServer}</h3>
+		</div>
+	)
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loggedIn, setLoggedIn] = useState(false)
+	const [user, setUser] = useState("");
+
+	const logout = () => {
+		facade.logout();
+		setLoggedIn(false)
+	}
+	const login = (user, pass) => {
+		facade.login(user, pass)
+			.then(res => setLoggedIn(true));
+		setUser(user);
+	}
+
+	return (
+		<div>
+			{!loggedIn ? (<LogIn login={login}/>) :
+				(<div>
+					<LoggedIn user={user}/>
+					<button onClick={logout}>Logout</button>
+				</div>)}
+		</div>
+	)
+
 }
 
 export default App;
