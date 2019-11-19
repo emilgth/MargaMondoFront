2a8ae3f3c447ea796892a2897859f13dc282f027
Emil <emilgth@gmail.com>
2019-11-19 18:22:37 +0100

    table now fetches from backend api

--- src/FlightsTable.js
+++ src/FlightsTable.js
@@ -4,15 +4,16 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import filterFactory from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import facade from "./apiFacade";
 
 export const FlightsTable = () => {
 	const [flights, setFlights] = useState([]);
 	const [msg, setMsg] = useState("");
 	const columns = [{
-		dataField: 'departureLocation',
+		dataField: 'departureAirportName',
 		text: 'Departure Location',
 	}, {
-		dataField: 'arrivalLocation',
+		dataField: 'arrivalAirportName',
 		text: 'Arrival Location',
 	}, {
 		dataField: 'departureTime',
@@ -32,44 +33,45 @@ export const FlightsTable = () => {
 	}];
 	useEffect(() => {
 		// setMsg("Loading...");
-		// setFlights(facade.fetchAllFlights().then(res => res.json()));
+		facade.fetchAllFlights().then(data => setFlights(data));
 		// setMsg("");
 
 		//todo fetch data from API
-		setFlights([
-			{
-				departureLocation: 'Copenhagen',
-				arrivalLocation: 'Paris',
-				departureTime: 213432,
-				arrivalTime: 12391238,
-				flightDuration: 231355,
-				price: 124,
-				airline: "FcukLnine"
-			},
-			{
-				departureLocation: 'Copenhagen',
-				arrivalLocation: 'Paris',
-				departureTime: 213432,
-				arrivalTime: 12391238,
-				flightDuration: 231355,
-				price: 124,
-				airline: "FcukLnine"
-			},
-			{
-				departureLocation: 'Copenhagen',
-				arrivalLocation: 'Paris',
-				departureTime: 213432,
-				arrivalTime: 12391238,
-				flightDuration: 231355,
-				price: 124,
-				airline: "FcukLnine"
-			}]);
+		// setFlights([
+		// 	{
+		// 		departureLocation: 'Copenhagen',
+		// 		arrivalLocation: 'Paris',
+		// 		departureTime: 213432,
+		// 		arrivalTime: 12391238,
+		// 		flightDuration: 231355,
+		// 		price: 124,
+		// 		airline: "FcukLnine"
+		// 	},
+		// 	{
+		// 		departureLocation: 'Copenhagen',
+		// 		arrivalLocation: 'Paris',
+		// 		departureTime: 213432,
+		// 		arrivalTime: 12391238,
+		// 		flightDuration: 231355,
+		// 		price: 124,
+		// 		airline: "FcukLnine"
+		// 	},
+		// 	{
+		// 		departureLocation: 'Copenhagen',
+		// 		arrivalLocation: 'Paris',
+		// 		departureTime: 213432,
+		// 		arrivalTime: 12391238,
+		// 		flightDuration: 231355,
+		// 		price: 124,
+		// 		airline: "FcukLnine"
+		// 	}]);
 	}, []);
 
 	return (
 		<div>
 			<h2>All Flights</h2>
 			{msg}
+			{console.log(flights)}
 			<BootstrapTable
 				striped
 				hover
