51b4d4335151b4da6797a90c60f0d84b5cc6fe78
Emil <emilgth@gmail.com>
2019-11-26 10:12:38 +0100

    now uses global api

--- src/App.js
+++ src/App.js
@@ -5,57 +5,57 @@ import {FlightSearch} from "./FlightSearch";
 import facade from "./apiFacade";
 //Welcome to the jungle
 const Welcome = () => {
-	return "Welcome to MargaMondo";
+    return "Welcome to MargaMondo";
 };
 
 const Header = () => {
-	return (
-		<div><NavLink to={"/"}>Home</NavLink>
-		</div>
-	)
+    return (
+        <div><NavLink to={"/"}>Home</NavLink>
+        </div>
+    )
 };
 
 function App() {
-	const [flights, setFlights] = useState([]);
+    const [flights, setFlights] = useState([]);
 
-	useEffect(() => {
-		facade.fetchAllFlights().then(data => {
-			//todo: fix typo in the global array >:)
-			let combinedArrays = [...data[0], ...data[1]];
-			let flightsFormattedTime = combinedArrays.map(flight => {
-				flight.flightDuration = msToTime(flight.flightDuration);
-				return flight;
-			});
-			setFlights(flightsFormattedTime)
-		});
-	}, []);
+    useEffect(() => {
+        facade.fetchAllFlights().then(data => {
+            //todo: fix typo in the global array >:)
+            let combinedArrays = [...data[0], ...data[1]];
+            let flightsFormattedTime = combinedArrays.map(flight => {
+                flight.flightDuration = msToTime(flight.flightDuration);
+                return flight;
+            });
+            setFlights(flightsFormattedTime)
+        });
+    }, []);
 
-	function msToTime(duration) {
-		let seconds = Math.floor((duration / 1000) % 60),
-			minutes = Math.floor((duration / (1000 * 60)) % 60),
-			hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    function msToTime(duration) {
+        let seconds = Math.floor((duration / 1000) % 60),
+            minutes = Math.floor((duration / (1000 * 60)) % 60),
+            hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-		hours = (hours < 10) ? "0" + hours : hours;
-		minutes = (minutes < 10) ? "0" + minutes : minutes;
-		seconds = (seconds < 10) ? "0" + seconds : seconds;
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+//todo change to 1 hours and 343 minutes
+        return hours + ":" + minutes + ":" + seconds;
+    }
 
-		return hours + ":" + minutes + ":" + seconds;
-	}
-
-	return (
-		<div>
-			<Router>
-				<Header/>
-				<Switch>
-					<Route exact path={"/"}>
-						<Welcome/>
-					</Route>
-				</Switch>
-				<FlightSearch setFlights={setFlights}/>
-				<FlightsTable flights={flights}/>
-			</Router>
-		</div>
-	);
+    return (
+        <div>
+            <Router>
+                <Header/>
+                <Switch>
+                    <Route exact path={"/"}>
+                        <Welcome/>
+                    </Route>
+                </Switch>
+                <FlightSearch setFlights={setFlights}/>
+                <FlightsTable flights={flights}/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
