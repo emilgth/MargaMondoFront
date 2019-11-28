f266667726a36274d76aa7aec5447015ea0acd71
Matias <buekoefoed@gmail.com>
2019-11-28 09:31:27 +0100

    Added price to ticket

--- src/App.js
+++ src/App.js
@@ -107,6 +107,7 @@ function App() {
 
 	const SelectedFlightsRenderer = () => {
 		if (selectedFlight && selectedReturnFlight) {
+			let returnPrice = selectedFlight.price + selectedReturnFlight.price;
 			return (
 				<div>
 					<h3>Your ticket</h3>
@@ -117,7 +118,7 @@ function App() {
 						{selectedReturnFlight.airline} {selectedReturnFlight.departureTime} {selectedReturnFlight.duration} {selectedReturnFlight.departureAirportCode}->{selectedReturnFlight.arrivalAirportCode}
 					</p>
 					<p>
-						<NavLink to="/redirecting">Redirect to booking</NavLink>
+						${returnPrice},- <NavLink to="/redirecting">Redirect to booking</NavLink>
 					</p>
 				</div>
 			)
@@ -129,7 +130,7 @@ function App() {
 						{selectedFlight.airline} {selectedFlight.departureTime} {selectedFlight.duration} {selectedFlight.departureAirportCode}->{selectedFlight.arrivalAirportCode}
 					</p>
 					<p>
-						<NavLink to="/redirecting">Redirect to booking</NavLink>
+						${selectedFlight.price},- <NavLink to="/redirecting">Redirect to booking</NavLink>
 					</p>
 				</div>
 			)
